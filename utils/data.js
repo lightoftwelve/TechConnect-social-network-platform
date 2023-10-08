acd8102f555b1197c8e724c727ba2fb6f1045acb
lightoftwelve <info@lightoftwelve.com>
2023-10-08 00:38:28 -0400

    added seed data to data.js

---
+++ utils/data.js
@@ -0,0 +1,127 @@
+const userData = [
+  {
+    username: "codeMaster",
+    email: "codeMaster@techconnect.com",
+  },
+  {
+    username: "bugHunter",
+    email: "bugHunter@techconnect.com",
+  },
+  {
+    username: "algoWiz",
+    email: "algoWiz@techconnect.com",
+  },
+  {
+    username: "dataDiva",
+    email: "dataDiva@techconnect.com",
+  },
+  {
+    username: "uiGenius",
+    email: "uiGenius@techconnect.com",
+  },
+  {
+    username: "aiAficionado",
+    email: "aiAficionado@techconnect.com",
+  },
+  {
+    username: "cryptoCritic",
+    email: "cryptoCritic@techconnect.com",
+  },
+  {
+    username: "pairPro",
+    email: "pairPro@techconnect.com",
+  },
+  {
+    username: "cloudCaptain",
+    email: "cloudCaptain@techconnect.com",
+  },
+  {
+    username: "containerCommander",
+    email: "containerCommander@techconnect.com",
+  },
+];
+
+const thoughtData = [
+  {
+    thoughtText:
+      "Just learned about GraphQL! It's so cool and solves so many problems that REST API has. What do you think?",
+    username: "codeMaster",
+  },
+  {
+    thoughtText: "Struggling with CSS Flexbox. Any tips on how to master it?",
+    username: "uiGenius",
+  },
+  {
+    thoughtText: "What's your favorite sorting algorithm and why?",
+    username: "algoWiz",
+  },
+  {
+    thoughtText:
+      "Do you prefer functional programming or object-oriented programming?",
+    username: "dataDiva",
+  },
+  {
+    thoughtText:
+      "Debugging can be frustrating but also so rewarding when you finally solve the problem!",
+    username: "bugHunter",
+  },
+  {
+    thoughtText:
+      "Been diving deep into machine learning lately. The potential of AI is both exciting and terrifying. Thoughts?",
+    username: "aiAficionado",
+  },
+  {
+    thoughtText:
+      "I've been thinking about the implications of quantum computing on current encryption methods. Will our data still be safe?",
+    username: "cryptoCritic",
+  },
+  {
+    thoughtText:
+      "Has anyone tried pair programming? Does it boost productivity or is it just a distraction?",
+    username: "pairPro",
+  },
+  {
+    thoughtText:
+      "Serverless architecture: A game-changer or just a buzzword? Would love to hear experiences from those who have implemented it.",
+    username: "cloudCaptain",
+  },
+  {
+    thoughtText:
+      "Docker and containerization have changed the way I deploy applications. Anyone else feel the same?",
+    username: "containerCommander",
+  },
+];
+
+const reactionsPool = [
+  { reactionBody: "This is amazing!", username: "uiGenius" },
+  { reactionBody: "I hadn't thought of it that way.", username: "codeMaster" },
+  { reactionBody: "Very insightful!", username: "dataDiva" },
+  { reactionBody: "Well said!", username: "algoWiz" },
+  { reactionBody: "I totally agree!", username: "bugHunter" },
+  { reactionBody: "Interesting perspective.", username: "aiAficionado" },
+  { reactionBody: "Thanks for sharing this.", username: "cryptoCritic" },
+  { reactionBody: "I was just thinking the same thing!", username: "pairPro" },
+  { reactionBody: "I'm not sure I understand.", username: "cloudCaptain" },
+  { reactionBody: "Can you elaborate?", username: "containerCommander" },
+  { reactionBody: "This made me think.", username: "uiGenius" },
+  { reactionBody: "I had a different take on this.", username: "algoWiz" },
+  { reactionBody: "I'd love to discuss this further.", username: "dataDiva" },
+  { reactionBody: "You bring up a good point.", username: "codeMaster" },
+  {
+    reactionBody: "That's an angle I hadn't considered.",
+    username: "bugHunter",
+  },
+  { reactionBody: "This is a hot topic right now.", username: "aiAficionado" },
+  {
+    reactionBody: "I've been reading up on this too.",
+    username: "cryptoCritic",
+  },
+  { reactionBody: "This deserves more attention.", username: "pairPro" },
+  { reactionBody: "I learned something new today!", username: "cloudCaptain" },
+  {
+    reactionBody: "You've changed my mind with this.",
+    username: "containerCommander",
+  },
+];
+
+module.exports = { userData, thoughtData, reactionsPool };
