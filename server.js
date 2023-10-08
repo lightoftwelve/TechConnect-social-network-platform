273952773c82b71d7f9ac90dcc8793ce4e8054e0
lightoftwelve <info@lightoftwelve.com>
2023-10-08 00:30:26 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const db = require("./config/connection");
+const routes = require("./routes");
+const errorHandler = require("./utils/errorHandler");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+app.use(errorHandler);
+
+db.on("error", (error) => {
+  console.error("MongoDB connection error:", error);
+});
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}!`);
+  });
+});
